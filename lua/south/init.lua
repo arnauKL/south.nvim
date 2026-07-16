840b94f4f441fadc21b6505b5d4ff1c2d17f0e79
arnauKL <arnaukdeprez@gmail.com>
2026-07-16 11:13:16 +0200

    added fzf-lua integration

--- lua/south/init.lua
+++ lua/south/init.lua
@@ -109,6 +109,32 @@ M.setup = function(opts)
         TelescopeSelection             = { bg = cp.selection },
         TelescopeMatching              = { fg = cp.cobalt, bold = true },
 
+        -- fzf-lua
+        FzfLuaNormal                   = { fg = cp.black, bg = float_bg },
+        FzfLuaBorder                   = { fg = cp.cool_medium_grey, bg = float_bg },
+        FzfLuaTitle                    = { fg = cp.denim, bg = float_bg, bold = true },
+        FzfLuaBackdrop                 = { bg = cp.black, blend = 20 }, -- Dim the background window slightly
+
+        FzfLuaPreviewNormal            = { link = "FzfLuaNormal" },
+        FzfLuaPreviewBorder            = { link = "FzfLuaBorder" },
+        FzfLuaPreviewTitle             = { fg = cp.lake, bg = float_bg, bold = true },
+
+        FzfLuaFzfPrompt                = { fg = cp.denim, bold = true },
+        FzfLuaFzfPointer               = { fg = cp.aqua, bold = true },
+        FzfLuaFzfMatch                 = { fg = cp.cobalt, bold = true },
+        FzfLuaFzfCursorLine            = { bg = cp.selection },
+        FzfLuaFzfScrollbar             = { fg = cp.cool_medium_grey },
+
+        FzfLuaHeaderBind               = { fg = cp.denim },
+        FzfLuaHeaderText               = { fg = cp.cool_dark_grey },
+        FzfLuaPathColNr                = { fg = cp.sky },
+        FzfLuaPathLineNr               = { fg = cp.grass },
+        FzfLuaBufNr                    = { fg = cp.denim },
+        FzfLuaBufFlagCur               = { fg = cp.auburn, bold = true },
+        FzfLuaBufFlagAlt               = { fg = cp.sky },
+        FzfLuaLivePrompt               = { fg = cp.denim, bold = true },
+        FzfLuaLiveSym                  = { fg = cp.purple, bold = true },
+
         -- Oil.nvim
         OilDir                         = { fg = cp.denim, bold = true },
         OilDirSlash                    = { fg = cp.cool_dark_grey },
